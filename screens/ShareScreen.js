49b2145749d39875a2a5ded1bbe8d38eb7d86d21
Ray Arvin Rimorin <rayarvin@gmail.com>
2020-04-04 21:20:31 +0800

    Update package.json, ShareScreen.js, and yarn.lock sharing

--- screens/ShareScreen.js
+++ screens/ShareScreen.js
@@ -1,9 +1,19 @@
 import * as React from "react";
-import { Avatar, Button, Card, Title, Paragraph, Surface } from "react-native-paper";
+import {
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  Surface,
+  Colors,
+  Appbar,
+} from "react-native-paper";
 import TopAppBar from "../navigation/AppBar";
 import { Image } from "react-native";
+import * as Sharing from "expo-sharing";
 
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />;
+const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
 const ShareScreen = ({ route, navigation }) => {
   if (!route.params) {
@@ -11,16 +21,36 @@ const ShareScreen = ({ route, navigation }) => {
   }
   const { height, localUri, uri, width } = route.params;
 
+  const openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+
+    Sharing.shareAsync(localUri);
+  };
+
   if (!height || !localUri || !uri || !width) {
     return <Title>No Image</Title>;
   }
   return (
     <Surface style={{ flex: 1 }}>
-      <TopAppBar />
+      <Appbar.Header>
+        <Appbar.Content title="Share your creation"/>
+      </Appbar.Header>
       <Image
-        style={{ width: '100%', height: undefined, aspectRatio:1 }}
+        style={{ width: "100%", height: undefined, aspectRatio: 1 }}
         source={{ uri: localUri }}
       />
+      <Button
+        style={{ margin: 20 }}
+        mode="contained"
+        icon="share"
+        color={Colors.amber900}
+        onPress={openShareDialogAsync}
+      >
+        Share This
+      </Button>
     </Surface>
   );
 };
