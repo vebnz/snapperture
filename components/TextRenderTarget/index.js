5b4a105e7402295cf921898c0178b4a14474b21a
Ray Arvin Rimorin <rayarvin@gmail.com>
2020-04-06 17:28:34 +0800

    Update index.js

--- components/TextRenderTarget/index.js
+++ components/TextRenderTarget/index.js
@@ -3,6 +3,7 @@ import { GLView } from "expo-gl";
 import React, { useRef, useState } from "react";
 import { Surface, Button, Colors } from "react-native-paper";
 import { Image } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 
 const TextRenderTarget = (props) => {
   const canvasRenderer = useRef()
@@ -37,6 +38,15 @@ const TextRenderTarget = (props) => {
           const snap = await canvasRenderer.current.takeSnapshotAsync();
           console.log("TextRenderTarget -> snap", snap);
           setRenderImg(snap.localUri);
+          const {
+            status,
+            permissions,
+          } = await MediaLibrary.requestPermissionsAsync();
+          if (status === "granted") {
+            return MediaLibrary.saveToLibraryAsync(snap.localUri);
+          } else {
+            throw new Error("CameraRoll permission not granted");
+          }
         }}
       >
         snap
